6f0a84f7ff90b729c3e8bdbc0931f43cd21b8814
ChocolateBandit <kadyrkulove@mail.ru>
2023-08-17 20:21:32 +0600

    create components folder Accordion and Rating. Then functions moved for component and export for App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {findAllByDisplayValue} from "@testing-library/react";
+import Accordion from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
 
 function App() {
   return (
@@ -16,42 +16,6 @@ function App() {
 function AppTitle() {
     return <h1>My app components_props_propstype</h1>
 }
-function Rating() {
-  return (
-      <>
-          <Star />
-          <Star />
-          <Star />
-      </>
-  )
-}
 
-function Star () {
-    return <div>Star</div>
-}
 
-function Accordion () {
-    return <div>
-        <AccordionTitle />
-        <AccordionBody />
-    </div>
-}
-
-function AccordionTitle () {
-    return (
-        <>
-            <h3>Menu</h3>
-        </>
-    )
-}
-
-function AccordionBody () {
-    return (
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
-    )
-}
 export default App;
